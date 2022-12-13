ec0168ff6a9c286ee87a87bae38ef70b79f07097
Bohdan Mykhalevych <b.mykhalevych@tidio.net>
2022-12-13 12:20:49 +0100

    Added config file. Added index route.

--- container.py
+++ container.py
@@ -4,8 +4,11 @@ from src.modules.project.infrastructure.containers import ProjectContainer
 
 
 class ApplicationContainer(containers.DeclarativeContainer):
+    config = providers.Configuration(ini_files=['config.ini'])
+
     wiring_config = containers.WiringConfiguration(modules=[
-        'src.modules.project.infrastructure.http.routes'
+        'src.modules.auth.infrastructure.http.routes',
+        'src.modules.project.infrastructure.http.routes',
     ])
 
     project = providers.Container(
--- src/modules/auth/infrastructure/http/routes.py
+++ src/modules/auth/infrastructure/http/routes.py
@@ -1,8 +1,19 @@
 
 from fastapi import APIRouter, status
+from dependency_injector.wiring import inject, Provide
+from container import ApplicationContainer
 from .models import AuthModel
 
 router = APIRouter()
+
+
+@router.get('/')
+@inject
+def index(version: str = Provide[ApplicationContainer.config.api.version]):
+    return {
+        "message": f'Version: {version}',
+        "code": status.HTTP_200_OK
+    }
 
 
 @router.post('/auth')
